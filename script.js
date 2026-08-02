3975d43acbb33179266747853250485c5c7cbd39
giftmshengu250-pixel <your.github.giftmshengu250@gmail.com>
2026-08-02 20:14:22 +0200

    added script

---
+++ script.js
@@ -0,0 +1,15 @@
+const bookingForm = document.querySelector('#bookingForm')
+let nameInput = document.querySelector('#nameInput')
+let phoneNumber = document.querySelector('#phoneNumber')
+let style = document.querySelector('#style')
+let submitButton = document.querySelector('#submission')
+
+function inputName () {
+    let nameValue = nameInput.value.toLowerCase();
+}
+function cellNumber () {
+    let phoneValue = phoneNumber.value.toLowerCase();
+}
+function hairTyle () {
+    let styleValue = style.value.toLowerCase();
+}
